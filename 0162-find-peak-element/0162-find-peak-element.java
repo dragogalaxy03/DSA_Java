6e9f3d1ec41200f314a15ce4f07395e9d162ca58
dragogalaxy03 <122115096+dragogalaxy03@users.noreply.github.com>
2025-02-04 19:29:50 +0530

    Time: 0 ms (100%), Space: 42.5 MB (55.76%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -11,7 +11,7 @@ class Solution {
             if((nums[mid]>nums[mid-1]) && (nums[mid]>nums[mid+1])) return mid;
             else if(nums[mid]>nums[mid-1]) low = mid+1;
             else if(nums[mid]>nums[mid+1]) high = mid-1;
-            else low = mid+1;
+            else high = mid-1;
         }
         return -1;
         
